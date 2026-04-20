893867ee2d6fa6332aaaa65e9d6334846c1a945b
M3l1l0t <psarenas6@gmail.com>
2026-04-20 14:48:09 +0200

    subo proyecto completo

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import "./Footer.css";
+
+const template = () => {
+  return `
+  <footer>
+    <p>©️ Created by Paloma Sánchez</p>
+  </footer>
+  `;
+};
+
+const Footer = () => {
+  return template();
+};
+
+export default Footer;
---
+++ src/components/data/data.js
@@ -0,0 +1,77 @@
+const data = {
+  name: "Paloma Sánchez",
+  address: "Madrid, Spain",
+  email: "psarenas@hotmail.es",
+  avatar:
+    "https://img.freepik.com/foto-gratis/renderizacion-personaje-anime-abeja_23-2150963632.jpg?semt=ais_hybrid&w=740&q=80",
+  aboutMe:
+    "I'm a passionate reader, animal lover, and language enthusiast with a strong curiosity for learning and personal growth. I enjoy exploring different cultures through languages and continuously expanding my knowledge through reading. My interests include studying new languages, understanding animal behavior, and developing a deeper appreciation for the world around me. I'm committed to lifelong learning and bringing empathy, curiosity, and dedication into everything I do.",
+
+  education: {
+    degree: "Full Stack Development",
+    university: "The Power",
+    graduationYear: 2026,
+    relevantCourses: [
+      "Web Development Fundamentals",
+      "Grado Superior en Gestión de Alojamientos Turísticos",
+      "Auxiliar de Veterinaria",
+      "Grado en Esudios Ingleses",
+    ],
+  },
+  workExperience: [
+    {
+      position: "Receptionist",
+      company: "Hotel Reception Services",
+      startDate: "2017",
+      endDate: "2026",
+      description:
+        "As a hotel receptionist, I managed guest check-ins and check-outs, handled reservations, and provided excellent customer service. I ensured smooth front desk operations and assisted guests with their needs to guarantee a positive and welcoming experience.",
+    },
+    {
+      position: "Veterinary Assistant",
+      company: "Veterinary Hospital",
+      startDate: "2014",
+      endDate: "2016",
+      description:
+        "As a veterinary assistant, I support veterinarians in daily clinical tasks, assist during examinations and treatments, and ensure the comfort and safety of animals. I am committed to providing compassionate care and maintaining a calm and professional environment for both patients and owners.",
+    },
+  ],
+  skills: [
+    "HTML5",
+    "CSS3",
+    "JavaScript",
+    "React",
+    "Node.js",
+    "Customer service",
+    "Communication skills",
+    "Customer service",
+  ],
+  projects: [
+    {
+      title: "Landing Page",
+      description:
+        "A modern and responsive landing page designed to present a product or service in a clear and engaging way. The project focuses on user experience, visual hierarchy, and clean design to effectively communicate key information and encourage user interaction or conversion. Built with a mobile-first approach and optimized for performance and accessibility.",
+      link: "https://github.com/M3l1l0t/PROYECTO-1",
+      preview:
+        "https://plus.unsplash.com/premium_photo-1684785618727-378a3a5e91c5?auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8ZWNvbW1lcmNlfGVufDB8fDB8fHww&w=500",
+    },
+    {
+      title: "Filter Store",
+      description:
+        "A modern e-commerce store specialized in filters, designed to provide a smooth and intuitive shopping experience. The platform allows users to easily browse, compare, and purchase different types of filters with a clean and responsive interface. Focused on usability, performance, and a seamless checkout process, the project ensures an efficient and user-friendly experience across all devices.",
+      link: "https://github.com/M3l1l0t/PROYECTO-2",
+      preview:
+        "https://plus.unsplash.com/premium_photo-1684581214880-2043e5bc8b8b?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2070",
+    },
+    {
+      title: "Pinterest Async",
+      description:
+        "An asynchronous Pinterest-style application designed to load and display content efficiently using API requests. The project focuses on infinite scrolling, dynamic data fetching, and smooth user experience. It allows users to browse and explore image-based content in a responsive grid layout, optimizing performance through async/await handling and lazy loading techniques.",
+      link: "https://github.com/M3l1l0t/PROYECTO_3",
+      preview:
+        "https://images.unsplash.com/photo-1487017159836-4e23ece2e4cf?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2071",
+    },
+  ],
+};
+
+export default data;
