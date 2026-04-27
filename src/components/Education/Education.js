7583d48a0a9932dcb2500e2537c55f87ccf5a172
M3l1l0t <155572645+M3l1l0t@users.noreply.github.com>
2026-04-27 14:02:32 +0200

    Education.js

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,31 +1,24 @@
 import data from "../../components/data/data";
 import "../Education/Education.css";
 
-const template = () => {
+const Education = () => {
   return `
-    <section class="education" id="education">
+    <section class="education">
       <h2>Education</h2>
+
       <h3>${data.education.degree}</h3>
       <h4>${data.education.university}</h4>
       <p>${data.education.graduationYear}</p>
+
       <h3>Relevant Courses</h3>
-      <ul id="relevantcourses">
-      ${data.education.relevantCourses
-        .map(
-          (course) => `
-      <li>
-        <p>${course}</p>
-      </li>
-      `
-        )
-        .join("")}
+
+      <ul class="relevantcourses">
+        ${data.education.relevantCourses
+          .map((course) => `<li><p>${course}</p></li>`)
+          .join("")}
       </ul>
     </section>
   `;
-};
-
-const Education = () => {
-  return template();
 };
 
 export default Education;
