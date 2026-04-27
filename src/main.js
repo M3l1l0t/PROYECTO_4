531dc98cf09b9f36c4ecb6250b2de40c219bb3e8
M3l1l0t <155572645+M3l1l0t@users.noreply.github.com>
2026-04-27 13:59:34 +0200

    Main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,23 @@
 import "./style.css";
+
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { Main, addMainListeners } from "./components/Main/Main";
 import { addAboutListeners } from "./components/AboutMe/AboutMe";
 
 const render = () => {
-  document.querySelector("#app").innerHTML = `
+  const app = document.querySelector("#app");
+
+  app.innerHTML = `
     ${Header()}
     ${Main()}
     ${Footer()}
   `;
+
+  requestAnimationFrame(() => {
+    addAboutListeners();
+    addMainListeners();
+  });
 };
 
 render();
-addAboutListeners();
-addMainListeners();
